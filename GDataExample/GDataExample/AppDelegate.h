e37f1ac33ca2b3f4ff148967d2ac200886199b8b
Kros <ch8908@gmail.com>
2014-02-02 12:02:05 +0800

    GData example first commit

---
+++ GDataExample/GDataExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GDataExample
+//
+//  Created by Huang ChienShuo on 2/2/14.
+//  Copyright (c) 2014 ThousandSuqare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GDataExample/GDataExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GDataExample
+//
+//  Created by Huang ChienShuo on 2/2/14.
+//  Copyright (c) 2014 ThousandSuqare. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
